17bdafcfd0bd90bc0c54dbfed934ef72a231c987
RiesHunter <Hries@wisc.edu>
2023-04-13 20:37:11 -0500

    Created nex

--- scripts/Ape.R
+++ scripts/Ape.R
@@ -17,8 +17,8 @@ if(!require(phangorn)){
 }
 
 #### Import data ####
-dir_s <- paste("/Users/rieshunter/Documents/bioinformatics/myProject/data/HK_1/parsed_fa")
-dir_muscle <- paste("/Users/rieshunter/Documents/bioinformatics/myProject/data/HK_1/parsed_fa/muscle")
+dir_s <- paste("/Users/rieshunter/Library/CloudStorage/GoogleDrive-hries@wisc.edu/Shared drives/TCF lab/Current Lab Members/Hunter_Ries/_Admin/Thesis/Coursework/Phylo/myProject/results")
+dir_muscle <- paste("/Users/rieshunter/Library/CloudStorage/GoogleDrive-hries@wisc.edu/Shared drives/TCF lab/Current Lab Members/Hunter_Ries/_Admin/Thesis/Coursework/Phylo/myProject/data/HK_1/parsed_fa/muscle")
 setwd(dir_muscle); getwd(); dir()
 dna_muscle_HA <- fasta2DNAbin(file="muscle_segmented_compiled-HA.fasta")
 phydat_muscle_HA <- read.phyDat(file="muscle_segmented_compiled-HA.fasta", format="fasta")
@@ -53,21 +53,25 @@ plotBS(midpoint(treeRatchet),
 add.scale.bar()
 
 #### Root ML tree ####
-dir <- paste("/home/rieshunter/Code/myProject/data/HK_1/parsed_fa/muscle/raxml-ng")
+dir <- paste("/Users/rieshunter/Library/CloudStorage/GoogleDrive-hries@wisc.edu/Shared drives/TCF lab/Current Lab Members/Hunter_Ries/_Admin/Thesis/Coursework/Phylo/myProject/data/HK_1/parsed_fa/muscle/raxml-ng")
 setwd(dir); getwd(); dir()
 ## .bestTree
 bestTree <- paste("HK_1_HA.raxml.bestTree")
-tr_bestTree <- ape::read.tree(bestTree)
+tr_bestTree <- read.tree(text = c('((HA|MH8714_A.removed.parsed:0.000572,(((((((HA|HS1566_B.removed.parsed:0.001146,(HA|HS1579_B.removed.parsed:0.015078,(HA|PC1A.removed.parsed:0.000596,HA|PC2B.removed.parsed:0.001123):0.001087):0.001817):0.000001,(HA|MH8924_A.removed.parsed:0.001128,(HA|HS1364_B.removed.parsed:0.000001,HA|HS1341_B.removed.parsed:0.000572):0.000934):0.000232):0.000001,HA|HS1409_B.removed.parsed:0.000001):0.000001,(HA|HS1345_B.removed.parsed:0.000572,HA|HS1417_B.removed.parsed:0.000572):0.000001):0.000001,(((HA|HS1425_B.removed.parsed:0.000572,HA|HS1357_B.removed.parsed:0.000001):0.001727,HA|HS1465_B.removed.parsed:0.001146):0.000577,(HA|HS1294_B.removed.parsed:0.000572,(((HA|MH8589_A.removed.parsed:0.001731,(HA|MH8453_B.removed.parsed:0.001145,HA|HS1518_B.removed.parsed:0.001720):0.000001):0.000001,HA|HS1562_B.removed.parsed:0.000001):0.000001,HA|HS1457_B.removed.parsed:0.001146):0.001722):0.000001):0.000001):0.000001,(HA|HS1573_B.removed.parsed:0.001146,HA|HS1532_B.removed.parsed:0.001722):0.000001):0.000001,(HA|MH9045_A.removed.parsed:0.001743,HA|MH9547_B.removed.parsed:0.001151):0.002302):0.000572):0.000001,((HA|MH8922_A.removed.parsed:0.001719,(HA|HS1530_B.removed.parsed:0.000001,(HA|HS1377_B.removed.parsed:0.001145,HA|HS1484_B.removed.parsed:0.001145):0.000001):0.000572):0.000001,(HA|HS1427_B.removed.parsed:0.000001,HA|HS1455_B.removed.parsed:0.000572):0.001147):0.000001,HA|HS1402_B.removed.parsed:0.000001);'))
+
+## clean labels
 tr_bestTree$tip.label <- gsub(".removed.parsed", "", tr_bestTree$tip.label)
 tr_bestTree$tip.label <- gsub("HA\\|", "", tr_bestTree$tip.label)
-tr_bestTree_root <- root(tr_bestTree, outgroup = "MH8714_A")
+is.rooted(tr_bestTree)
+
+tr_bestTree_root <- root(tr_bestTree, outgroup = 1, resolve.root = TRUE)
+ # rooting to MH8714_A, the first node
+is.rooted(tr_bestTree_root)
 plot(tr_bestTree_root)
 
-## .mlTrees
-mlTrees <- paste("HK_1_HA.raxml.mlTrees")
-tr_mlTrees <- lapply(ape::read.tree(mlTrees, keep.multi = T), ape::consensus)
-plot(tr_mlTrees)
+## tree to nexus format
+setwd(dir_s); getwd(); dir()
+write.nexus(tr_bestTree_root, file = "HK_1.nex")
 
-
-
-
+## double-check that it looks good
+plot(read.nexus("HK_1.nex"))
