bc16c3ff28de429da4027472599904b4cb878570
RiesHunter <Hries@wisc.edu>
2023-05-02 22:34:00 -0500

    Plots

--- scripts/MLtree_root_score.R
+++ scripts/MLtree_root_score.R
@@ -19,6 +19,10 @@ if(!require(phytools)){
   install.packages("phytools", dependencies = T)
   library(phytools)
 }
+if(!require(ggtree)){
+  install.packages("ggtree", dependencies = T)
+  library(ggtree)
+}
 
 #### Import data ####
 dir_s <- paste("/Users/rieshunter/Library/CloudStorage/GoogleDrive-hries@wisc.edu/Shared drives/TCF lab/Current Lab Members/Hunter_Ries/_Admin/Thesis/Coursework/Phylo/myProject/figures")
@@ -38,15 +42,21 @@ clean_tips <- function(x) {
   is.rooted(x)
   return(x)
 }
-root_tree <- function (x) {
-  #x <- root(x, outgroup = 1, resolve.root = TRUE)
-  x <- midpoint.root(x)
+root_tree <- function (x, y) {
+  x <- root(x, outgroup = y, 
+            resolve.root = TRUE)
+  #x <- midpoint.root(x)
   print(is.rooted(x))
   return(x)
 }
 likelihood <- function(x, y) {
   temp <- pml(x, phydat, site.rate = "gamma", model = "GTR")
   temp2 <- data.frame("logLik" = temp$logLik, "group" = y)
+  return(temp2)
+}
+treelength <- function(x, y) {
+  temp <- sum(node.depth.edgelength(x))
+  temp2 <- data.frame("treelength" = temp, "group" = y)
   return(temp2)
 }
 
@@ -60,9 +70,9 @@ tr_bestTree <- read.tree(bestTree)
 phydat <- read.phyDat(file="clustalw_segmented_compiled-HA.fasta", format="fasta")
 phydat <- clean_cols(phydat)
 tr_bestTree <- clean_tips(tr_bestTree)
-ClustalW <- root_tree(tr_bestTree)
-plot(ClustalW)
+ClustalW <- root_tree(tr_bestTree, "MH8156_A")
 df_likelihood <- likelihood(ClustalW, "HK_c_r")
+df_treelength <- treelength(ClustalW, "HK_c_r")
 
 #Muscle through RAxML-NG
 bestTree <- paste("HK_muscle.raxml.bestTree")
@@ -70,12 +80,28 @@ tr_bestTree <- read.tree(bestTree)
 phydat <- read.phyDat(file="muscle_segmented_compiled-HA.fasta", format="fasta")
 phydat <- clean_cols(phydat)
 tr_bestTree <- clean_tips(tr_bestTree)
-Muscle <- root_tree(tr_bestTree)
-plot(Muscle)
+Muscle <- root_tree(tr_bestTree,"MH8156_A")
 df_likelihood <- rbind(likelihood(Muscle, "HK_m_r"),df_likelihood)
+df_treelength <- rbind(treelength(ClustalW, "HK_m_r"),df_treelength)
 
 ## compare trees
 plot_HK <- comparePhylo(ClustalW, Muscle, plot = T, use.edge.length = T)
+
+wasp.cophylo<-cophylo(ClustalW,Muscle)
+plot(wasp.cophylo,link.type="curved",
+     link.lwd=4,
+     link.lty="solid",
+     link.col=make.transparent("red", 0.25))
+title(main="Hong Kong",font.main=3)
+
+ggtree(Muscle) + 
+  geom_tiplab(size = 2, offset = 0.00125, hjust = 1) + 
+  theme_tree2() +
+  labs(title = "Hong Kong\nMuscle")
+ggtree(ClustalW) + 
+  geom_tiplab(size = 2, offset = 0.00125, hjust = 1) + 
+  theme_tree2() +
+  labs(title = "Hong Kong\nClustalW")
 
 #### cali09 ####
 ## import
@@ -87,9 +113,9 @@ tr_bestTree <- read.tree(bestTree)
 phydat <- read.phyDat(file="clustalw_segmented_compiled-HA.fasta", format="fasta")
 phydat <- clean_cols(phydat)
 tr_bestTree <- clean_tips(tr_bestTree)
-ClustalW <- root_tree(tr_bestTree)
-plot(ClustalW)
+ClustalW <- root_tree(tr_bestTree, "1336")
 df_likelihood <- rbind(likelihood(ClustalW, "cali09_c_r"),df_likelihood)
+df_treelength <- rbind(treelength(ClustalW, "cali09_c_r"),df_treelength)
 
 #Muscle through RAxML-NG
 bestTree <- paste("cali09_muscle.raxml.bestTree")
@@ -97,12 +123,28 @@ tr_bestTree <- read.tree(bestTree)
 phydat <- read.phyDat(file="muscle_segmented_compiled-HA.fasta", format="fasta")
 phydat <- clean_cols(phydat)
 tr_bestTree <- clean_tips(tr_bestTree)
-Muscle <- root_tree(tr_bestTree)
-plot(Muscle)
+Muscle <- root_tree(tr_bestTree, "1336")
 df_likelihood <- rbind(likelihood(Muscle, "cali09_m_r"),df_likelihood)
+df_treelength <- rbind(treelength(ClustalW, "cali09_m_r"),df_treelength)
 
 ## compare trees
 plot_cali09 <- comparePhylo(ClustalW, Muscle, plot = T, use.edge.length = T)
+
+wasp.cophylo<-cophylo(ClustalW,Muscle)
+plot(wasp.cophylo,link.type="curved",
+     link.lwd=4,
+     link.lty="solid",
+     link.col=make.transparent("red", 0.25))
+title(main="Cali09",font.main=3)
+
+ggtree(Muscle) + 
+  geom_tiplab(size = 2, offset = 0.00125, hjust = 1) + 
+  theme_tree2() +
+  labs(title = "Cali09\nMuscle")
+ggtree(ClustalW) + 
+  geom_tiplab(size = 2, offset = 0.00125, hjust = 1) + 
+  theme_tree2() +
+  labs(title = "Cali09\nClustalW")
 
 #### perth ####
 ## import
@@ -114,9 +156,9 @@ tr_bestTree <- read.tree(bestTree)
 phydat <- read.phyDat(file="clustalw_segmented_compiled-HA.fasta", format="fasta")
 phydat <- clean_cols(phydat)
 tr_bestTree <- clean_tips(tr_bestTree)
-ClustalW <- root_tree(tr_bestTree)
-plot(ClustalW)
+ClustalW <- root_tree(tr_bestTree, "Perth_pool")
 df_likelihood <- rbind(likelihood(ClustalW, "perth_c_r"),df_likelihood)
+df_treelength <- rbind(treelength(ClustalW, "perth_c_r"),df_treelength)
 
 #Muscle through RAxML-NG
 bestTree <- paste("perth_muscle.raxml.bestTree")
@@ -124,13 +166,28 @@ tr_bestTree <- read.tree(bestTree)
 phydat <- read.phyDat(file="muscle_segmented_compiled-HA.fasta", format="fasta")
 phydat <- clean_cols(phydat)
 tr_bestTree <- clean_tips(tr_bestTree)
-Muscle <- root_tree(tr_bestTree)
-plot(Muscle)
+Muscle <- root_tree(tr_bestTree, "Perth_pool")
 df_likelihood <- rbind(likelihood(Muscle, "perth_m_r"),df_likelihood)
+df_treelength <- rbind(treelength(ClustalW, "perth_m_r"),df_treelength)
 
 ## compare trees
 plot_perth <- comparePhylo(ClustalW, Muscle, plot = T, use.edge.length = T)
 
+wasp.cophylo<-cophylo(ClustalW,Muscle)
+plot(wasp.cophylo,link.type="curved",
+     link.lwd=4,
+     link.lty="solid",
+     link.col=make.transparent("red", 0.25))
+title(main="Perth",font.main=3)
+
+ggtree(Muscle) + 
+  geom_tiplab(size = 2, offset = 0.00125, hjust = 1) + 
+  theme_tree2() +
+  labs(title = "Perth\nMuscle")
+ggtree(ClustalW) + 
+  geom_tiplab(size = 2, offset = 0.00125, hjust = 1) + 
+  theme_tree2() +
+  labs(title = "Perth\nClustalW")
 
 #### Stats ####
 ## df_run
@@ -153,6 +210,18 @@ df_likelihood$da <- ""
 df_likelihood$da <- paste(df_likelihood$dataset, df_likelihood$alignment, sep = "_")
 df_likelihood$da <- as.factor(df_likelihood$da)
 df_likelihood$logLik <- round(df_likelihood$logLik, 4)
+
+## df_treelength
+df_treelength <- separate(df_treelength, "group", c("dataset", "alignment", "ml"), sep = "_")
+df_treelength$dataset <- as.factor(df_treelength$dataset)
+df_treelength$alignment <- gsub("m", "MUSCLE", df_treelength$alignment)
+df_treelength$alignment <- gsub("c", "ClustalW", df_treelength$alignment)
+df_treelength$alignment <- as.factor(df_treelength$alignment)
+df_treelength$da <- ""
+df_treelength$da <- paste(df_treelength$dataset, df_treelength$alignment, sep = "_")
+df_treelength$da <- as.factor(df_treelength$da)
+df_treelength$treelength <- round(df_treelength$treelength, 4)
+
 
 #### Plot ####
 setwd(dir_s); getwd(); dir()
